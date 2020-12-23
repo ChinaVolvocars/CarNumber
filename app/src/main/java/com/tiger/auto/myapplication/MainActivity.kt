e6f53a167bb4bc7031d56881c3e01d62bf768b1b
yuexiaohui <yuexiaohui@lvchengfuwu.com>
2020-12-23 11:11:38 +0800

    Initial commit

---
+++ app/src/main/java/com/tiger/auto/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tiger.auto.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), LicensePlateView.InputListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        activity_lpv.setInputListener(MainActivity@ this)
+
+        activity_lpv.setKeyboardContainerLayout(main_rl_container)
+//        activity_lpv.showLastView()
+        activity_lpv.hideLastView()
+        activity_lpv.onSetTextColor(R.color.colorAccent)
+    }
+
+    override fun inputComplete(content: String?) {
+    }
+
+    override fun deleteContent() {
+    }
+
+
+}
