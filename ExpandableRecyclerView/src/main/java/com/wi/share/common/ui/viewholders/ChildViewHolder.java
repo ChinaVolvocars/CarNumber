6f5af911ebcd763ba52d01746097a997720884e0
yuexiaohui <yuexiaohui@lvchengfuwu.com>
2020-12-25 09:27:48 +0800

    code

---
+++ ExpandableRecyclerView/src/main/java/com/wi/share/common/ui/viewholders/ChildViewHolder.java
@@ -0,0 +1,18 @@
+package com.wi.share.common.ui.viewholders;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wi.share.common.ui.models.ExpandableGroup;
+
+
+/**
+ * ViewHolder for {@link ExpandableGroup#items}
+ */
+public class ChildViewHolder extends RecyclerView.ViewHolder {
+
+    public ChildViewHolder(View itemView) {
+        super(itemView);
+    }
+}
